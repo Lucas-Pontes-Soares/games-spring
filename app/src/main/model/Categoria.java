ef8ccbae15206d897bbc38bbcc755e67af034ebc
Lucas Pontes Soares <eterneru.lucas@outlook.com>
2025-02-12 18:02:51 +0000

    Primeira Aula models

---
+++ app/src/main/model/Categoria.java
@@ -0,0 +1,24 @@
+import java.util.HashSet;
+import java.util.Set;
+
+package application.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.OneToMany;
+
+@Entity
+@Table(name = "categorias")
+public class Categoria {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private long id;
+    @Column(unique=true, nullable=false)
+
+    private String nome;
+    
+}
