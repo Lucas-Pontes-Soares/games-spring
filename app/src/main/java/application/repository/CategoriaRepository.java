07159320485e2d63d8c2bb5377a3619b579453b0
Lucas Pontes Soares <eterneru.lucas@outlook.com>
2025-02-17 17:55:17 +0000

    Adicionando primeiros arquivos

---
+++ app/src/main/java/application/repository/CategoriaRepository.java
@@ -0,0 +1,9 @@
+package application.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import apllication.model.Categoria;
+
+public interface CategoriaRepository extends CrudRepository<Categoria, Long>{
+
+}
